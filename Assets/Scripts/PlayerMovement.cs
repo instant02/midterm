415d066fe49da098ce6c932fac7a6cff763d247c
instant02 <youngmin@gim-yeongmins-MacBook-Pro.local>
2025-10-29 16:02:39 +0900

    final

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -17,8 +17,6 @@ public class PlayerMovement : MonoBehaviour
     
     private float m_JumpCooldown = 0.5f; 
     private float m_LastJumpTime = -0.5f; 
-
-    
     private bool m_JumpInput;
     private bool m_IsRunningInput;
 
@@ -39,8 +37,6 @@ public class PlayerMovement : MonoBehaviour
         {
             m_JumpInput = true;
         }
-
-       
         m_IsRunningInput = Input.GetKey(KeyCode.LeftShift);
     }
 
@@ -51,21 +47,15 @@ public class PlayerMovement : MonoBehaviour
        
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-
         m_Movement.Set(horizontal, 0f, vertical);
         m_Movement.Normalize();
 
-        
         bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
         m_Animator.SetBool("IsWalking", isWalking);
-
-        
         bool isRunning = isWalking && m_IsRunningInput;
         m_Animator.SetBool("IsRunning", isRunning);
-
-       
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);
 
@@ -87,26 +77,16 @@ public class PlayerMovement : MonoBehaviour
 
     private void HandleJumping()
     {
-     
         if (m_JumpInput)
         {
-            
             bool canJump = (Time.time - m_LastJumpTime) >= m_JumpCooldown;
-
-          
             if (canJump)
             {
                 
                 m_Animator.SetTrigger("Jump");
-
-                
                 m_Rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-
-              
                 m_LastJumpTime = Time.time;
             }
-
-          
             m_JumpInput = false;
         }
     }
@@ -116,12 +96,10 @@ public class PlayerMovement : MonoBehaviour
 
         float speed = m_Animator.deltaPosition.magnitude;
 
- 
         if (m_Animator.GetBool("IsRunning"))
         {
             speed *= runSpeedMultiplier;
         }
-
         m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * speed);
         m_Rigidbody.MoveRotation(m_Rotation);
     }
